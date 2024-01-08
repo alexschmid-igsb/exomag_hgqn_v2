517f64b90499670c401202b1da32b9e6ea333688
Alex Schmid <schmida@uni-bonn.de>
2024-01-08 12:26:42 +0100

    commit

---
+++ backend/database/init_mongodb_for_exomag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -sp "Password for MongoDB user 'exomag_dev': " pwd_exomag_dev
+echo
+read -sp "Password for MongoDB user 'exomag_production': " pwd_exomag_production
+echo
+
+mongosh \
+   -u root \
+   -quiet \
+   --eval "use exomag_dev;" \
+   --eval "db.dropDatabase();" \
+   --eval "use exomag_dev;" \
+   --eval "db.createUser({ user:'exomag_dev', pwd: '$pwd_exomag_dev', roles:[{db:'exomag_dev', role:'readWrite'}]});" \
+   --eval "db.createCollection('dummy');" \
+   --eval "use exomag_production;" \
+   --eval "db.dropDatabase();" \
+   --eval "use exomag_production;" \
+   --eval "db.createUser({ user:'exomag_production', pwd: '$pwd_exomag_production', roles:[{db:'exomag_production', role:'readWrite'}]});" \
+   --eval "db.createCollection('dummy');" \
+
