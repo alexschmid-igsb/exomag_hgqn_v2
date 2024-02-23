e37476f8ce85110fdcf3be679b639840bdf98b45
Alex Schmid <alexschmid@gmx.de>
2024-02-23 13:02:57 +0100

    anpassung und korrektur email templates

--- frontend/src/util/mail/base/templates/ActivationAdmin.jsx
+++ frontend/src/util/mail/base/templates/ActivationAdmin.jsx
@@ -16,6 +16,8 @@ import {
 } from '@react-email/components'
 
 import styles from './Styles'
+import Logo from './Logo'
+
 
 import {interpolate as T} from './LodashTemplateMask'
 
@@ -24,7 +26,7 @@ const URL = () => {
     if(process.env.REACT_APP_HOST.startsWith('localhost')) {
         return `http://${process.env.REACT_APP_HOST}/activation/{{=token}}`
     } else {
-        return `https://www.${process.env.REACT_APP_HOST}/activation/{{=token}}</> `
+        return `https://${process.env.REACT_APP_HOST}/activation/{{=token}}`
     }
 }
 
@@ -37,7 +39,7 @@ export const ActivationAdmin = () => {
             <Body style={styles.body}>
                 <Container style={styles.container}>
                     <Section style={styles.logo}>
-                        LOGO
+                        <Logo/>
                     </Section>
                     <Section style={styles.content} >
                         <Text style={styles.paragraph}>Dear User,</Text>
--- frontend/src/util/mail/base/templates/Styles.js
+++ frontend/src/util/mail/base/templates/Styles.js
@@ -7,12 +7,18 @@ const styles = {
 
     body: {
         backgroundColor: '#efeef1',
+        backgroundColor: 'white',
         fontFamily,
     },
 
+    // #0B4F6C
+    // rgba(255, 255, 255, 0.8)
+
     container: {
         width: '580px',
-        margin: '30px auto'
+        margin: '30px auto',
+        boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+        borderRadius: '3px',
     },
 
     paragraph: {
@@ -36,10 +42,12 @@ const styles = {
         display: 'flex',
         justifyContent: 'center',
         alingItems: 'center',
-        padding: '30px',
-        backgroundColor: '#BCA',
+        padding: '20px',
+        // backgroundColor: '#8AAAC7',
+        // backgroundColor: '#FFFFFF',
+        backgroundColor: '#F0F0F0',
         border: border,
-        borderBottom: 'none',
+        borderBottom: '1px dashed rgba(0,0,0,0.2)',
         borderBottomLeftRadius: '0px',
         borderBottomRightRadius: '0px',
     },
