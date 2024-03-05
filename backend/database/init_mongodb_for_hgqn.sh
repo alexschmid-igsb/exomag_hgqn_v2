de435b48bd02cec711a568d60e24b52d6757928f
Alex Schmid <schmida@uni-bonn.de>
2024-03-05 17:08:50 +0100

    commit

--- backend/database/init_mongodb_for_hgqn.sh
+++ backend/database/init_mongodb_for_hgqn.sh
@@ -7,17 +7,17 @@ echo
 
 mongosh \
    -u root \
-   -quiet \
    --eval "use hgqn_dev;" \
-   --eval "db.dropDatabase();" \
-   --eval "db.dropUser('hgqn_dev');" \
+   --eval "try { db.dropDatabase(); } catch(err) { console.log(err.message) }" \
+   --eval "try { db.dropUser('hgqn_dev'); } catch(err) { console.log(err.message) }" \
    --eval "use hgqn_dev;" \
    --eval "db.createUser({ user:'hgqn_dev', pwd: '$pwd_hgqn_dev', roles:[{db:'hgqn_dev', role:'readWrite'}]});" \
    --eval "db.createCollection('dummy');" \
    --eval "use hgqn_production;" \
-   --eval "db.dropDatabase();" \
-   --eval "db.dropUser('hgqn_production');" \
+   --eval "try { db.dropDatabase(); } catch(err) { console.log(err.message) }" \
+   --eval "try { db.dropUser('hgqn_production'); } catch(err) { console.log(err.message) }" \
    --eval "use hgqn_production;" \
    --eval "db.createUser({ user:'hgqn_production', pwd: '$pwd_hgqn_production', roles:[{db:'hgqn_production', role:'readWrite'}]});" \
    --eval "db.createCollection('dummy');" \
+
 
